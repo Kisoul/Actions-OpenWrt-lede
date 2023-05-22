75e028917e067c2f283df66cb6cd924cb577d79a
Kisoul <32985553+Kisoul@users.noreply.github.com>
2023-05-22 10:13:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,14 +13,20 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 # Modify default DISTRIB_REVISION
-sed -i 's/R23.5.1/R23.5.1 (Compiled by Kisoul on 05.19.2023)/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R23.5.1/R23.5.1 (Compiled by Kisoul on 05.22.2023)/g' package/lean/default-settings/files/zzz-default-settings
+# Fix Transmission-web-control
+echo "#修复Transmission-web-control" > fix_file
+echo "sed -i 's#procd_add_jail transmission log#procd_add_jail_mount \"$web_home\"#g' /etc/init.d/transmission" >> fix_file
+cat fix_file >> package/lean/default-settings/files/zzz-default-settings
+rm fix_file
 # Modify Openwrt's Port Forward
-echo "#修改wan为静态地址\"192.168.2.200\"&修改物理接口为\"eth2\"" > new_file
-echo "sed -i '/eth1/,\$d' /etc/config/network" >> new_file
-echo "echo \"	option ifname 'eth2'\" >> /etc/config/network" >> new_file
-echo "echo \"	option proto 'static'\" >> /etc/config/network" >> new_file
-echo "echo \"	option ipaddr '192.168.2.200'\" >> /etc/config/network" >> new_file
-echo "echo \"	option netmask '255.255.255.0'\" >> /etc/config/network" >> new_file
-echo "echo \"	option gateway '192.168.2.1'\" >> /etc/config/network" >> new_file
-echo "echo \"	option dns '192.168.2.1'\" >> /etc/config/network" >> new_file
-
+echo "#修改wan为静态地址\"192.168.2.200\"&修改物理接口为\"eth2\"" > temp_file
+echo "sed -i '/eth1/,\$d' /etc/config/network" >> temp_file
+echo "echo \"	option ifname 'eth2'\" >> /etc/config/network" >> temp_file
+echo "echo \"	option proto 'static'\" >> /etc/config/network" >> temp_file
+echo "echo \"	option ipaddr '192.168.2.200'\" >> /etc/config/network" >> temp_file
+echo "echo \"	option netmask '255.255.255.0'\" >> /etc/config/network" >> temp_file
+echo "echo \"	option gateway '192.168.2.1'\" >> /etc/config/network" >> temp_file
+echo "echo \"	option dns '192.168.2.1'\" >> /etc/config/network" >> temp_file
+cat temp_file >> /etc/config/network
+rm temp_file
