d47879e0ed1f04909e63ba1ca9526f7436190fbc
Kisoul <32985553+Kisoul@users.noreply.github.com>
2023-12-06 14:15:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,13 +21,14 @@ cat fix_file >> package/lean/default-settings/files/zzz-default-settings
 rm fix_file
 # Modify Openwrt's Port Forward
 echo "#修改wan为静态地址\"192.168.2.200\"&修改物理接口为\"eth2\"" > temp_file
-echo "sed -i '/eth1/,\$d' /etc/config/network" >> temp_file
-echo "echo \"	option ifname 'eth2'\" >> /etc/config/network" >> temp_file
-echo "echo \"	option proto 'static'\" >> /etc/config/network" >> temp_file
-echo "echo \"	option ipaddr '192.168.2.200'\" >> /etc/config/network" >> temp_file
-echo "echo \"	option netmask '255.255.255.0'\" >> /etc/config/network" >> temp_file
-echo "echo \"	option gateway '192.168.2.1'\" >> /etc/config/network" >> temp_file
-echo "echo \"	option dns '192.168.2.1'\" >> /etc/config/network" >> temp_file
+echo "sed -i '/wan/,\$d' /etc/config/network" >> temp_file
+echo "echo \"config interface 'wan'\" >> /etc/config/network" >> temp_file
+echo "echo \" 	 option ifname 'eth2'\" >> /etc/config/network" >> temp_file
+echo "echo \" 	 option proto 'static'\" >> /etc/config/network" >> temp_file
+echo "echo \" 	 option ipaddr '192.168.2.200'\" >> /etc/config/network" >> temp_file
+echo "echo \" 	 option netmask '255.255.255.0'\" >> /etc/config/network" >> temp_file
+echo "echo \" 	 option gateway '192.168.2.1'\" >> /etc/config/network" >> temp_file
+echo "echo \" 	 option dns '192.168.2.1'\" >> /etc/config/network" >> temp_file
 cat temp_file >> package/lean/default-settings/files/zzz-default-settings
 rm temp_file
 # Fix the conflict between lede and ssmall-package
