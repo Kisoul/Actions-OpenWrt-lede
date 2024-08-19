64f7e19d4118ff1a80238cbc0c4cf263b14d5657
Kisoul <32985553+Kisoul@users.noreply.github.com>
2024-08-19 14:15:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,9 +18,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source （openwrt-packages is release version; small-package is beta version; small is passwall Library dependencies;）
-#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
