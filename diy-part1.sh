c72860c8b868f59e1e114445a9f3a22855cf4bb1
Kisoul <32985553+Kisoul@users.noreply.github.com>
2024-08-20 10:29:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source （openwrt-packages is release version; small-package is beta version; small is passwall Library dependencies;）
+# Add a feed source （openwrt-packages is release version; small-package is beta version; small is passwall Library dependencies, golang is smpackage compilation tools;）
 #sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
